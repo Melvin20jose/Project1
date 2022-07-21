99ee6ccb3925ef12f29c9a1a9ad451547f3b880a
Melvin20jose <melvin20jose@gmail.com>
2022-07-21 20:27:00 +0530

    Jose code update

--- src/org/array/LinkedList.java
+++ src/org/array/LinkedList.java
@@ -7,27 +7,25 @@ import java.util.Vector;
 
 public class LinkedList {
 	public static void main(String[] args) {
-    List l=new ArrayList<>() ;
-    l.add(10);
-    l.add(20);
-    l.add(30);
-    l.add(40);
-    l.add(50);
-    l.add(60);
-    l.add(70);
-    l.add(80);
-    System.out.println(l);
-    l.add(3,35);
-    System.out.println(l);
-    l.remove(2);
-    System.out.println(l);
-<<<<<<< HEAD
-    System.out.println("Jose added coding");
-    
-=======
-    System.out.println("Melvin added code");
->>>>>>> ef9e57e76e8e41d0221d0add520ede3956f87be3
-    
+		List l = new ArrayList<>();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(40);
+		l.add(50);
+		l.add(60);
+		l.add(70);
+		l.add(80);
+		System.out.println(l);
+		l.add(3, 35);
+		System.out.println(l);
+		l.remove(2);
+		System.out.println(l);
+
+		System.out.println("Melvin added code");
+
+		System.out.println("Jose added coding");
+
 	}
 
 }
